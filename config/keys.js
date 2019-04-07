9807ad2080074c1c951bd0f84a3f5ed7e93ba2a8
Abousaid Salah-Eddine <abousaid7salah@gmail.com>
2019-04-07 16:30:19 +0200

    make it better

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://salah:salah1442@ds243768.mlab.com:43768/mern_shopping'
+}
---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MOngo
+mongoose.connect(db, {
+  useNewUrlParser: true
+})
+  .then(() => console.log('MongoDB Connected ...'))
+  .catch(err => console.log(err));
+
+  
+  const port = process.env.PORT || 5000;
+  
+  // Cors Middleware
+  // app.use(cors());
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accespt, Authorization');
+    if(req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+    }
+    next();
+  });
+
+  // Serve static assets if in production
+  if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+      res.sendStatus(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
+  
+  // Use Routes
+  app.use('/api/items', items);
+
+app.listen(port, () => console.log(`Server started on port ${port}.`));
+
+// npm cache clean --force
+
